0e41790f95d2df0564c5cf45f17e3a1e3a111114
Davis Miculis <mindzhulis@gmail.com>
2016-11-23 23:30:23 +0200

    Added method chaining and payment testing API URL.

--- src/G2APay.php
+++ src/G2APay.php
@@ -2,17 +2,19 @@
 
 /**
  * G2APay PHP Library
- * @author  	Kacper "karer" Geisheimer
- * @copyright 	Copyright (c) 2016 Kacper Geisheimer
+ * @author  	Davis Miculis, <mindzhulis@gmail.com>
+ * @copyright 	Copyright (c) 2016 Davis Miculis
  * @license 	https://opensource.org/licenses/MIT The MIT License (MIT)
  */
 
 namespace G2APay;
 
-class G2A_Payment
+class G2APay
 {
 	const API_URL = 'https://checkout.pay.g2a.com';
+	const API_TEST_URL = 'https://checkout.test.pay.g2a.com';
 
+	private $apiUrl;
 	private $apiHash;
 	private $apiSecret;
 	private $urlSuccess;
@@ -23,50 +25,66 @@ class G2A_Payment
 
 	public function __construct($apiHash, $apiSecret, $urlSuccess, $urlFail, $orderId, $currency = 'USD')
 	{
-		$this->apiHash 		= $apiHash;
-		$this->apiSecret 	= $apiSecret;
-		$this->urlSuccess 	= $urlSuccess;
-		$this->urlFail		= $urlFail;
-		$this->orderId 		= $orderId;
-		$this->currency 	= $currency;
+		$this->apiUrl = self::API_URL;
+		$this->apiHash = $apiHash;
+		$this->apiSecret = $apiSecret;
+		$this->urlSuccess = $urlSuccess;
+		$this->urlFail = $urlFail;
+		$this->orderId = $orderId;
+		$this->currency = $currency;
 	}
 
-	public function addItem($sku, $name, $quanity, $id, $price, $url, $extra = '', $type = '')
+	public function addItem($sku, $name, int $quantity, $id, float $price, $url, $extra = '', $type = '')
 	{
 		$this->items[] = [
-			'sku'		=> $sku,
-			'name'		=> $name,
-			'amount'	=> $quanity * $price,
-			'qty'	=> $quanity,
-			'id'		=> $id,
-			'price'		=> $price,
-			'url'		=> $url,
-			'extra'		=> $extra,
-			'type'		=> $type,
+			'sku' => $sku,
+			'name' => $name,
+			'amount' => floatval($quantity * $price),
+			'qty' => $quantity,
+			'id' => $id,
+			'price' => $price,
+			'url' => $url,
+			'extra' => $extra,
+			'type' => $type,
 		];
+
+		return $this;
+	}
+
+	public function test()
+	{
+		$this->apiUrl = self::API_TEST_URL;
+
+		return $this;
 	}
 
 	public function create($extra = array())
 	{
-		// Calculate total price of items.
+		// Temporary save api url, then reset to default
+		$url = $this->apiUrl;
+		$this->apiUrl = self::API_URL;
+
+		// Calculate total price of items
 		$amount = array_sum(array_column($this->items, 'amount'));
 
-		// Prepare array with data to query G2A.
+		// Prepare array with data to query G2A
 		$fields = array_merge([
 			'api_hash'		=> $this->apiHash,
 			'hash'			=> $this->calculateHash($amount),
 			'order_id'		=> $this->orderId,
 			'amount'		=> $amount,
 			'currency'		=> $this->currency,
+			// 'description'	=> '',
+			// 'email'			=> '',
 			'url_failure'	=> $this->urlFail,
 			'url_ok'		=> $this->urlSuccess,
 			'items'			=> $this->items,
 		], $extra);
 
-		// Request API server.
+		// Request API server
 		$ch = curl_init();
-		
-		curl_setopt($ch, CURLOPT_URL, self::API_URL . '/index/createQuote');
+
+		curl_setopt($ch, CURLOPT_URL, $url.'/index/createQuote');
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_POST, true);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($fields));
@@ -74,14 +92,14 @@ class G2A_Payment
 
 		$response = curl_exec($ch);
 		curl_close($ch);
-		
-		// Convert response from JSON format to native PHP array.
+
+		// Convert response from JSON text to PHP object/array
 		$result = json_decode($response);
 
 		if (isset($result->token)) {
 			return [
 				'success' => true,
-				'url' => (self::API_URL . '/index/gateway?token=' . $result->token)
+				'url' => ($url.'/index/gateway?token='.$result->token)
 			];
 		} else {
 			return [
@@ -93,6 +111,6 @@ class G2A_Payment
 
 	private function calculateHash($amount)
 	{
-		return hash('sha256', $this->orderId . number_format($amount, 2) . $this->currency . $this->apiSecret);
+		return hash('sha256', $this->orderId.number_format($amount, 2).$this->currency.$this->apiSecret);
 	}
 }
